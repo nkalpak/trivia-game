c7a9455a47f5320a7d8fe230305bbbbbcc24bf6f
nkalpak <nikitakalpakovski1@outlook.com>
2020-10-30 13:53:47 +0100

    Removed a function, inlined the code

--- src/components/game/components/game-over/game-over.tsx
+++ src/components/game/components/game-over/game-over.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { GameState } from '../../../../global/types';
 import { Limelight } from '../../../limelight/limelight';
 import { HeadingStyled } from '../../../app/app-styles';
 import { Button } from '../../../button/button';
@@ -12,14 +11,11 @@ export const GameOver = () => {
   const { score, state } = useGameContext();
   const history = useHistory();
 
-  const gameOverText = (s: GameState) => {
-    if (s === 'win') return 'You won!';
-    return 'You lost. Try again?';
-  };
-
   return (
     <GameOverStyled>
-      <HeadingStyled size={25}>{gameOverText(state)}</HeadingStyled>
+      <HeadingStyled size={25}>
+        {state === 'win' ? 'You won!' : 'You lost. Try again?'}
+      </HeadingStyled>
 
       <LimelightWrapperStyled>
         <Limelight>
